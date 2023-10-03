f98727827f91c0457dd0e02f0f0bde88aecc78a0
kip2 <tqa1no4@gmail.com>
2023-10-03 21:56:42 +0900

    add: FoodItemの枠を作成

---
+++ src/FoodItems/FoodItem.php
@@ -0,0 +1,18 @@
+<?php
+
+abstract class FoodItem {
+    private string $name;
+    // 説明
+    private string $description;
+
+    private float $price;
+
+    public static function getCategory() {
+
+    }
+
+
+
+    
+
+}
