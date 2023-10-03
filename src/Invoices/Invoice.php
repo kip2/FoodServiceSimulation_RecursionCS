3d5267e269b290fb8025ef0785e45849552b4b02
kip2 <tqa1no4@gmail.com>
2023-10-03 22:04:22 +0900

    add: Invoiceに枠を追加

---
+++ src/Invoices/Invoice.php
@@ -0,0 +1,10 @@
+<?php
+class Invoice {
+    private float $finalPrice;
+
+    // todo: Timestamp型は何で定義するのか？
+    private Timestamp $orderTime;
+
+    // todo: 何をするメソッド？
+    private int $estimatedTimeInMinutes;
+}
