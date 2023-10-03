8f6a4f7dc24be1f836f3fe2586465df266fec104
kip2 <tqa1no4@gmail.com>
2023-10-03 22:45:27 +0900

    add: Customerの枠を追加

---
+++ src/Persons/Customers/Customer.php
@@ -0,0 +1,9 @@
+<?php
+
+class Customer extends Person {
+    public function interestedCategories(Restaurant $restaurant): array {
+        // todo: エラーを解消する最低限のコード
+        return ["", ""];
+    }
+
+}
