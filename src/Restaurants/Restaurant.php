82bdcb53f18c3da8f8124924cc3f0ad64fec0dd4
kip2 <tqa1no4@gmail.com>
2023-10-03 22:37:00 +0900

    add: Restaurantの枠を追加

--- src/Restaurants/Restaurant.php
+++ src/Restaurants/Restaurant.php
@@ -1,5 +1,16 @@
 <?php
 
 class Restaurant {
+    /** @var FoodItem[] */
+    private array $menu = [];
+
+    /** @var Employee[] */ 
+    private array $employee = [];
+
+    public function order(string ...$categories): Invoice{
+        // todo: とりあえずエラーなしのコード
+        $invoice = new Invoice();
+        return $invoice;
+    }
 
 }
