dfab66f17c9a197c07c459fbff58b14a9db88acd
kip2 <tqa1no4@gmail.com>
2023-10-04 12:21:59 +0900

    add: food系クラスに、クラス名を返す処理を追加

--- src/FoodItems/CheeseBurger.php
+++ src/FoodItems/CheeseBurger.php
@@ -5,8 +5,15 @@ require 'FoodItem.php';
 class CheeseBurger extends FoodItem {
     // todo: 中身は何がいる？
 
-    public function test() {
-        echo "hello";
+    public function __toString(){
+        return __CLASS__;
     }
 
+    // public function test() {
+    //     echo "hello";
+    // }
+
 }
+
+// $burger = new CheeseBurger();
+// echo $burger;
--- src/FoodItems/Spaghetti.php
+++ src/FoodItems/Spaghetti.php
@@ -2,4 +2,7 @@
 class Spaghetti extends FoodItem {
     // todo: 中身は何がいる？
 
+    public function __toString(){
+        return __CLASS__;
+    }
 }
