191c715b7b6a12d4b99a083535c0a8ab11064c7c
kip2 <tqa1no4@gmail.com>
2023-10-14 23:15:31 +0900

    fix: add docstring

--- src/Other/Timestamp.php
+++ src/Other/Timestamp.php
@@ -10,18 +10,32 @@ class Timestamp {
         $this->generated_time = time();
     }
     
-    // インスタンス生成時の時間を取得する
+    /**
+     * インスタンス生成時の時間を返す
+     *
+     * @return string
+     */
     public function getTime() : string{
         return date("Y/m/d H:i:s", $this->generated_time);
     }
 
-    // 引数の時刻と、インスタンス生成時の時刻の差を文字列にして返す
+    /**
+     * 引数の時刻と、インスタンス生成時の時刻の差を文字列にして返す
+     *
+     * @param integer $time
+     * @return string
+     */
     public function getElapsedTime(int $time) :string {
         $elapsedTime = $time - $this->generated_time;
         return date("i:s", $elapsedTime);
     }
 
-    public function __toString() {
+    /**
+     * 文字列に変換したgenerated_timeを返す
+     *
+     * @return string
+     */
+    public function __toString():string {
         return (string)$this->generated_time;
     }
 }
